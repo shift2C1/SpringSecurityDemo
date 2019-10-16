232c2cd73914321425f2ec0fec311b1512068c6f
wangchaogit config --global user.name wangchao <shift2C1@163.com>
2019-10-16 17:04:59 +0800

    退出登录

---
+++ src/main/java/com/pipichao/test/TestLogoutSuccess.java
@@ -0,0 +1,17 @@
+package com.pipichao.test;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestLogoutSuccess implements LogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        System.out.println("退出登录成功");
+        httpServletResponse.getWriter().write("logout success");
+    }
+}
