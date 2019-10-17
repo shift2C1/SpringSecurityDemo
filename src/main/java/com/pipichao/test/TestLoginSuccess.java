0f4dac944b697167939723f6c52a04f96bcbc5d9
wangchaogit config --global user.name wangchao <shift2C1@163.com>
2019-10-17 14:49:26 +0800

    新增jdbc dao userservice

--- src/main/java/com/pipichao/test/TestLoginSuccess.java
+++ src/main/java/com/pipichao/test/TestLoginSuccess.java
@@ -1,17 +1,24 @@
 package com.pipichao.test;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class TestLoginSuccess  implements AuthenticationSuccessHandler {
 
     @Override
-    public void onAuthenticationSuccess(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
         System.out.println("处理认证中");
 //        httpServletResponse.getWriter().write("登录认证成功");
+        UserDetails userDetails=(UserDetails) authentication.getPrincipal();
+        System.out.println(userDetails.getUsername());
+        System.out.println(userDetails.getPassword());
+        System.out.println(userDetails.getAuthorities());
         System.out.println("认证成功");
         httpServletResponse.sendRedirect("/index.jsp");
     }
